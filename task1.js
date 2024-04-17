89961367ed26b3c09e6cf34a2e16b36da10353f5
nsavi <112178048+nsavi@users.noreply.github.com>
2024-04-17 19:43:06 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e0e0e0',
-    justifyContent: 'center', 
-    alignItems: 'center', 
-  },
-  nameText: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.nameText}>Im N Sakthi Avinash</Text>
-      <Button title="Click Me!" onPress={() => console.log('Button pressed!')} />
+      <Text style={styles.nameText}>n. sakthi avinash</Text>
+      <Text style={styles.regNoText}>Reg. No.: 22mia1105</Text>
+      <Button title="Click Me!" onPress={() => alert('Button Pressed!')} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  regNoText: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
 
 export default App;
